c8ac0c7dce70ba3f166bb924efe5e444e39ac87f
M <M@DESKTOP-K0HAN3Q>
2020-06-14 20:28:03 +0200

    Add project files.

---
+++ Flags/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace Flags
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.flagBoxOne = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.flagBoxOne)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // flagBoxOne
+            // 
+            this.flagBoxOne.Location = new System.Drawing.Point(13, 13);
+            this.flagBoxOne.Name = "flagBoxOne";
+            this.flagBoxOne.Size = new System.Drawing.Size(229, 174);
+            this.flagBoxOne.TabIndex = 0;
+            this.flagBoxOne.TabStop = false;
+            this.flagBoxOne.Click += new System.EventHandler(this.flagBoxOne_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(489, 108);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Testclick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.flagBoxOne);
+            this.Name = "Form1";
+            this.Text = " ";
+            ((System.ComponentModel.ISupportInitialize)(this.flagBoxOne)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox flagBoxOne;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Flags/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+
+namespace Flags
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        string json = @"{""key1"":""value1"",""key2"":""value2""}";
+        Dictionary<string, string> values = JsonConvert.DeserializeObject<Dictionary<string, string>>(json);
+
+
+        public void DisplayFlag(String code)
+        {
+            Image flag = Image.FromFile($".\\country-flags-master\\png250px\\{code}.png");
+            flagBoxOne.Image = flag;
+            flagBoxOne.Width = flag.Width;
+            flagBoxOne.Height = flag.Height;
+
+        }
+
+
+        private void flagBoxOne_Click(object sender, EventArgs e)
+        {
+            Image flag = Image.FromFile(@".\country-flags-master\png250px\nl.png");
+            flagBoxOne.Image = flag;
+            flagBoxOne.Width = flag.Width;
+            flagBoxOne.Height = flag.Height;
+        }
+
+        private void Testclick(object sender, EventArgs e)
+        {
+            String code = "es";
+            DisplayFlag(code);
+        }
+    }
+}
