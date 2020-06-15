8818939d6c1efff7eee3b5537403140616ee6250
M <M@DESKTOP-K0HAN3Q>
2020-06-15 14:03:11 +0200

    added files

--- Flags/Form1.Designer.cs
+++ Flags/Form1.Designer.cs
@@ -30,12 +30,15 @@
         {
             this.flagBoxOne = new System.Windows.Forms.PictureBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.CountriesLabel = new System.Windows.Forms.Label();
+            this.FlagLabel = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.flagBoxOne)).BeginInit();
             this.SuspendLayout();
             // 
             // flagBoxOne
             // 
-            this.flagBoxOne.Location = new System.Drawing.Point(13, 13);
+            this.flagBoxOne.Location = new System.Drawing.Point(227, 36);
             this.flagBoxOne.Name = "flagBoxOne";
             this.flagBoxOne.Size = new System.Drawing.Size(229, 174);
             this.flagBoxOne.TabIndex = 0;
@@ -52,17 +55,49 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.Testclick);
             // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(294, 281);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // CountriesLabel
+            // 
+            this.CountriesLabel.AutoSize = true;
+            this.CountriesLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CountriesLabel.Location = new System.Drawing.Point(12, 12);
+            this.CountriesLabel.Name = "CountriesLabel";
+            this.CountriesLabel.Size = new System.Drawing.Size(99, 24);
+            this.CountriesLabel.TabIndex = 3;
+            this.CountriesLabel.Text = "Countries";
+            // 
+            // FlagLabel
+            // 
+            this.FlagLabel.AutoSize = true;
+            this.FlagLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FlagLabel.Location = new System.Drawing.Point(223, 9);
+            this.FlagLabel.Name = "FlagLabel";
+            this.FlagLabel.Size = new System.Drawing.Size(51, 24);
+            this.FlagLabel.TabIndex = 4;
+            this.FlagLabel.Text = "Flag";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.FlagLabel);
+            this.Controls.Add(this.CountriesLabel);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.flagBoxOne);
             this.Name = "Form1";
             this.Text = " ";
+            this.Load += new System.EventHandler(this.form_load);
             ((System.ComponentModel.ISupportInitialize)(this.flagBoxOne)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -70,6 +105,9 @@
 
         private System.Windows.Forms.PictureBox flagBoxOne;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label CountriesLabel;
+        private System.Windows.Forms.Label FlagLabel;
     }
 }
 
--- Flags/Form1.cs
+++ Flags/Form1.cs
@@ -20,23 +20,56 @@ namespace Flags
         }
 
 
-        string json = @"{""key1"":""value1"",""key2"":""value2""}";
-        Dictionary<string, string> values = JsonConvert.DeserializeObject<Dictionary<string, string>>(json);
+        static string json = File.ReadAllText(@"..\..\country-flags-master/countries.json");
+        static Dictionary<string, string> country_codes = JsonConvert.DeserializeObject<Dictionary<string, string>>(json);
+
+        List<string> country_png = country_codes.Keys.ToList();
+        List<string> country_name = country_codes.Values.ToList();
+
+        ComboBox CountryBox = new ComboBox();
+
+
+
+        private void form_load(object sender, EventArgs e)
+        {
+            
+            Controls.Add(CountryBox);
+            CountryBox.Location = new System.Drawing.Point(12, 36);
+            CountryBox.Width = 200;
+            foreach (string element in country_name)
+            {
+                CountryBox.Items.Add(element);  
+            }
+            CountryBox.SelectedIndexChanged += new System.EventHandler(CountryBox_SelectedIndexChanged);
+        }
+
+        private void CountryBox_SelectedIndexChanged(object sender, System.EventArgs e)
+        {
+            DisplayFlag(CountryToCode(CountryBox.Text));
+             
+        }
+
 
 
         public void DisplayFlag(String code)
         {
-            Image flag = Image.FromFile($".\\country-flags-master\\png250px\\{code}.png");
+            Image flag = Image.FromFile($"..\\..\\country-flags-master\\png250px\\{code}.png");
             flagBoxOne.Image = flag;
             flagBoxOne.Width = flag.Width;
             flagBoxOne.Height = flag.Height;
 
         }
 
+        public string CountryToCode(string country)
+        {
+            int i = country_name.IndexOf(country);
+            return country_png[i];
+        }
+
 
         private void flagBoxOne_Click(object sender, EventArgs e)
         {
-            Image flag = Image.FromFile(@".\country-flags-master\png250px\nl.png");
+            Image flag = Image.FromFile(@"..\..\country-flags-master\png250px\nl.png");
             flagBoxOne.Image = flag;
             flagBoxOne.Width = flag.Width;
             flagBoxOne.Height = flag.Height;
@@ -46,6 +79,9 @@ namespace Flags
         {
             String code = "es";
             DisplayFlag(code);
+            textBox1.Text = (country_name.Count).ToString();
         }
+
+        
     }
 }
